c68372534af80a8ebcdebb5a43cfa8400e95371c
Anuvab Chakraborty <anuvab.xd@gmail.com>
2022-09-22 02:34:25 +0530

    Accepted 231A

--- CODEFORCES PROBLEMS IN PYTHON/231 A. Team.py
+++ CODEFORCES PROBLEMS IN PYTHON/231 A. Team.py
@@ -1,14 +1,18 @@
 def team(z):
     count=0
-    for i in z:
-        if sum(i)>=2:
-            count+=1
+    #for i in z:
+        #if sum(i)>=2:
+            #count+=1
+    if sum(z)>=2:count+=1
     return count
 
 n=int(input())
-l=[]
+#l=[]
+z=0
 for i in range(n):
     m=list(map(int,input().split()))
-    l+=[m]
+    z+=team(m)
+    #l+=[m]
+print(z)
 #print(l)
-print(team(l))
+#print(team(l))
