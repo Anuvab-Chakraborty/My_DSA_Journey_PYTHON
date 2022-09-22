21385cc2a6e7ff7c80a9cbb8a19caf2576d703fd
Anuvab Chakraborty <anuvab.xd@gmail.com>
2022-09-22 23:10:10 +0530

    Accepted 158A

--- CODEFORCES PROBLEMS IN PYTHON/158A.Next Round.py
+++ CODEFORCES PROBLEMS IN PYTHON/158A.Next Round.py
@@ -1,3 +1,14 @@
-m,n=input().split()
+def nextRound(z,n):
+    c=0
+    for i in z:
+        if i>=z[n-1] and i>0:
+            c+=1
+    return c
+m,n=list(map(int,input().split()))
+#print(m)
+#print(n)
+
 for i in range(m):
-    z=list(map(int,input()))
+    z=list(map(int,input().split()))
+    print(nextRound(z,n))
+    break
