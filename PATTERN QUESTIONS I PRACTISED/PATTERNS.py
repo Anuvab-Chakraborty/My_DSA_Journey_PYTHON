cc6e70f2a848d76279d99b34e87cf6a9708d928e
Anuvab Chakraborty <anuvab.xd@gmail.com>
2022-09-29 02:25:28 +0530

    PATTERNS FOR INTERVIEWS

--- PATTERN QUESTIONS I PRACTISED/PATTERNS.py
+++ PATTERN QUESTIONS I PRACTISED/PATTERNS.py
@@ -24,6 +24,28 @@ def square_number(n):
             print(j,end=" ")
         print("")
 
+
+def square_same_number(n):
+    for i in range(n):
+        for j in range(n):
+            print("4",end=" ")
+        print("")
+
+
+
+def square_row_wise(n):
+    for i in range(1,n+1):
+        for j in range(n):
+            print(i,end=" ")
+        print("")
+
+
+def star_patterns_reverse(n):
+    for i in range(n):
+        for j in range(n,i,-1):
+            print("*",end=" ")
+        print("")
+
 n=4
 #normal_tri(n)
 """OUTPUT:
@@ -63,6 +85,43 @@ OUTPUT:
 1 2 3 4 
 1 2 3 4 
 1 2 3 4 
+
+"""
+
+#square_same_number(n)
+
+"""
+OUTPUT:
+
+4 4 4 4 
+4 4 4 4 
+4 4 4 4 
+4 4 4 4 
+
+"""
+
+#square_row_wise(n)
+
+"""
+OUTPUT:
+
+1 1 1 1 
+2 2 2 2 
+3 3 3 3 
+4 4 4 4
+
+"""
+
+
+#star_patterns_reverse(n)
+
+"""
+OUTPUT:
+
+* * * * 
+* * * 
+* * 
+*
 
 """
 
