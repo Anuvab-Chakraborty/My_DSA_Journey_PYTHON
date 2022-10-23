4e38eeb8a1a71ebc32fdc29bf47056bd8c4128bb
Anuvab Chakraborty <anuvab.xd@gmail.com>
2022-10-23 08:20:48 +0530

    Solution to 705A Added

--- CODEFORCES PROBLEMS IN PYTHON/705A Hulk.py
+++ CODEFORCES PROBLEMS IN PYTHON/705A Hulk.py
@@ -3,14 +3,14 @@ s1="I hate it"
 s2="I love it"
 s3="I hate that"
 s4="I love that"
-for i in range(1,n+1):
-    if i==n:
-        if i % 2==0:
+for char in range(1, n + 1):
+    if char==n:
+        if char % 2==0:
             print(s2,end=" ")
         else:
             print(s1,end=" ")
         break
-    if i%2==0:
+    if char%2==0:
         print(s4,end=" ")
     else:
         print(s3,end=" ")
