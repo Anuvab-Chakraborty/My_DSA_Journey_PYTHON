0bc47cb847955c4255fc8e543bc66e18220f200d
Anuvab Chakraborty <anuvab.xd@gmail.com>
2022-10-23 08:25:04 +0530

    Solution to 1352A updated

--- CODEFORCES PROBLEMS IN PYTHON/1352A.py
+++ CODEFORCES PROBLEMS IN PYTHON/1352A.py
@@ -1,15 +1,15 @@
 #1352A
 def solve(m):
-    l=[]
+    li=[]
     i=0
     while m>0:
         c=m%10
         m=m//10
         if c!=0:
-            l.append(c*(10**i))
+            li.append(c*(10**i))
         i+=1
-    print(len(l))
-    for i in l:
+    print(len(li))
+    for i in li:
         print(i,end=" ")
     print()
             
