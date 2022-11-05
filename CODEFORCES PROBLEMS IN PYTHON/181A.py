49c51cc30a79f8fff6c9974ca11c652a7e80e122
Anuvab Chakraborty <anuvab.xd@gmail.com>
2022-11-06 01:50:07 +0530

    181A added

--- CODEFORCES PROBLEMS IN PYTHON/181A.py
+++ CODEFORCES PROBLEMS IN PYTHON/181A.py
@@ -1,1 +1,10 @@
 #181A
+n,m = map(int, input().split())
+x,y = 0,0
+for i in range(n):
+    line = input()
+    for j in range(m):
+        if line[j] == "*":
+            x^=i
+            y^=j
+print(x+1,y+1)
