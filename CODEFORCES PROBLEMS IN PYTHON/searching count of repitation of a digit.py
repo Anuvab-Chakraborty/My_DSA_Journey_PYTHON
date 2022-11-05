c177585397f3dfda53c03d36233b070e86a822b3
Anuvab Chakraborty <anuvab.xd@gmail.com>
2022-11-06 02:00:27 +0530

    EXCESS PROBLEMS DELETED

--- CODEFORCES PROBLEMS IN PYTHON/searching count of repitation of a digit.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-num=123228
-toSearch="2"
-num=str(num)
-dic=Counter(num)
-#print(dic)
-for key in dic.keys():
-    if key==toSearch:
-        print(dic[key])
--- CODEFORCES PROBLEMS IN PYTHON/string.py
+++
@@ -1,7 +0,0 @@
-s="here in this sentence , test is present"
-#s="here in this sentence ,is not present"
-z="test"
-if s.find(z)==-1:print("Not here")
-else:
-    res=s.split(z,1)
-    print(res[1])
