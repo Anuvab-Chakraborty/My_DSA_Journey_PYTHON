f9e17b3efdbfdc1221ff93067785aecfd10d3aa3
Anuvab Chakraborty <anuvab.xd@gmail.com>
2022-11-11 05:58:02 +0530

    1605A added

---
+++ CODEFORCES PROBLEMS IN PYTHON/1605A.py
@@ -0,0 +1,14 @@
+#1605A
+for _ in range(int(input())):
+    a1,a2,a3=map(int,input().split())
+    d=abs((a1+a3)-2*a2)
+    if d%3==0:
+        print(0)
+    else:print(1)
+#1605A
+for _ in range(int(input())):
+    a1,a2,a3=map(int,input().split())
+    d=abs((a1+a3)-2*a2)
+    if d%3==0:
+        print(0)
+    else:print(1)
