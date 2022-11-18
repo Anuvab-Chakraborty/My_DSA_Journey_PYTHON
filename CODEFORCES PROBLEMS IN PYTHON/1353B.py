d6cc0f3cc66565d91872bc481501c83ae053c01e
Anuvab Chakraborty <anuvab.xd@gmail.com>
2022-11-18 16:11:12 +0530

    Optimized 1353B

--- CODEFORCES PROBLEMS IN PYTHON/1353B.py
+++ CODEFORCES PROBLEMS IN PYTHON/1353B.py
@@ -1,4 +1,5 @@
 #1353B
+"""BRUTE FORCE BELOW
 for _ in range(int(input())):
     n,k=map(int,input().split())
     a=list(map(int,input().split()))
@@ -8,4 +9,22 @@ for _ in range(int(input())):
     for i in range(k):
         if b[i]>a[i]:
             b[i],a[i]=a[i],b[i]
+    print(sum(a))
+"""
+"""OPTIMIZED CODE"""
+#1353B
+for _ in range(int(input())):
+    n,k=map(int,input().split())
+    a=list(map(int,input().split()))
+    b=list(map(int,input().split()))
+    while k>0:
+        p=a.index(min(a))
+        q=b.index(max(b))
+        #print(p,q)
+        if min(a)>=max(b):
+            break
+        else:
+            a[p],b[q]=b[q],a[p]
+        k-=1
+    #print(a)
     print(sum(a))
