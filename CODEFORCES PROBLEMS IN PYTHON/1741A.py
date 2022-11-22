6052327ab7b885955d03f7b29bae6c47957b80a5
Anuvab Chakraborty <anuvab.xd@gmail.com>
2022-11-22 21:35:20 +0530

    1741A added

---
+++ CODEFORCES PROBLEMS IN PYTHON/1741A.py
@@ -0,0 +1,42 @@
+for _ in range(int(input())):
+    a, b = map(str, input().split())
+    if "S" in a and "S" in b:
+        a = a.split("S");b = b.split("S")
+        a = "".join(a);b = "".join(b)
+        if len(a) > len(b):
+            print("<")
+        elif len(a) == len(b):
+            print("=")
+        else:
+            print(">")
+    elif "S" in a and "S" not in b:
+        print("<")
+    elif "S" not in a and "S" in b:
+        print(">")
+    elif "M" in a and "M" in b:
+        print("=")
+
+    elif "M" in a and "M" not in b:
+        if "L" in b:
+            print("<")
+        else:
+            print(">")
+    elif "M" not in a and "M" in b:
+        if "L" in a:
+            print(">")
+        else:
+            print("<")
+    elif "L" in a and "L" in b:
+        a = a.split("L");b = b.split("L")
+        a = "".join(a);b = "".join(b)
+        if len(a) > len(b):
+            print(">")
+        elif len(a) == len(b):
+            print("=")
+        else:
+            print("<")
+
+    elif "L" in a and "L" not in b:
+        print(">")
+    elif "L" not in a and "L" in b:
+        print("<")
