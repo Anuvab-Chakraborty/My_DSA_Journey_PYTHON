e563489ee424728f8242842b176ee54418a8afb9
Anuvab Chakraborty <anuvab.xd@gmail.com>
2022-11-26 23:15:12 +0530

    1296B added

---
+++ CODEFORCES PROBLEMS IN PYTHON/1296B.py
@@ -0,0 +1,95 @@
+# 1296B
+# ------------------- fast io --------------------
+import os
+import sys
+from io import BytesIO, IOBase
+import math
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+
+# ------------------- fast io --------------------
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+    return x
+
+
+def lcm(a, b): return a * b // gcd(a, b)
+
+
+def is_prime(n):
+    if (n <= 1):
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if (n % i == 0):
+            return False
+    return True
+
+
+# ------------------- write code from here --------------------
+def solve(n):
+    c_b = n // 10
+    left = n % 10
+    spent = c_b * 10
+    ttl = c_b + left
+    while ttl > 9:
+        c_b = ttl // 10
+        left = ttl % 10
+        spent += c_b * 10
+        ttl = c_b + left
+
+    return (spent + ttl)
+
+
+for _ in range(int(input())):
+    n = int(input())
+    print(solve(n))
