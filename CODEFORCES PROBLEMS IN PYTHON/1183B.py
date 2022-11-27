907d34c85959977bfa1001500da8b451bfca634a
Anuvab Chakraborty <anuvab.xd@gmail.com>
2022-11-28 03:18:51 +0530

    1183B added

---
+++ CODEFORCES PROBLEMS IN PYTHON/1183B.py
@@ -0,0 +1,7 @@
+#1183B
+for _ in range(int(input())):
+    n,k=map(int,input().split())
+    l=sorted(list(map(int,input().split())))
+    z=l[0]+k;ans=-1
+    if l[-1]-z>k:print(ans)
+    else:print(z)
